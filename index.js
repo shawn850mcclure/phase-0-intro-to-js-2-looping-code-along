18f71152607660eee422ea00e331e32a815fb1cc
shawnmcclure <shawn850mcclure@gmail.com>
2023-09-02 19:02:28 -0500

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name =  (["Guadalupe", "Ollie", "Ake"]);
+function writeCards(name){
+    const name2 = []
+    let i = 0;
+    while (i < name.length){
+        console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        name2.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+        i++
+    }
+    return name2
+}
+function countDown(numbers){
+    if(numbers >= 0){
+        console.log(numbers);
+        numbers--;
+        countDown(numbers)
+    }
+}
